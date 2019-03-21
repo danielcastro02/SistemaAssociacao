ab5a1716dea81068f72dc89d65e6acfdbea6e6fd
Konrado <Konrado@DESKTOP-VHO1N4T.lan>
2019-03-21 20:18:57 -0300

    v4.0

--- index.php
+++ index.php
@@ -1,17 +1,9 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
+        <title>Home</title>
         <?php
-        // put your code here
+        include_once './Bases/header.php';
         ?>
-    </body>
-</html>
+
+    </head>
