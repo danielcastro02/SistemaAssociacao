03127aba2cc1fc3aef9ff1b52c9066a53f6acff4
Daniel Zanini de Castro <zanini.castro@hotmail.com>
2019-03-21 17:40:19 -0300

    Add files via upload

---
+++ Base/header.php
@@ -0,0 +1,12 @@
+<?php
+if (realpath("./index.php")) {
+    $pontos = "";
+} else {
+    $pontos = ".";
+}
+?>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">   
+<link rel="stylesheet" href="<?php echo $pontos; ?>./css/materialize.css">
+<link rel="stylesheet" href="<?php echo $pontos; ?>./css/custom.css">
+<script type="text/javascript" src="<?php echo $pontos; ?>./js/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="<?php echo $pontos; ?>./js/materialize.js"></script>
