8b82721a9e34272aa640c4f59ffd7d84e128a5c2
Lucas Lima <lucaslimasilva1998@gmail.com>
2019-03-21 21:04:55 -0300

    1.0.2

--- Tela/cadastroUsuario.php
+++ Tela/cadastroUsuario.php
@@ -37,13 +37,28 @@ session_start();
                             <label for="cidade">Cidade</label>
                         </div>
                         <div class="input-field col s12">
-                            
+                            <input class="input-field" type="text" name="bairro">
+                            <label for="bairro">Bairro</label>
                         </div>
                         <div class="input-field col s12">
-
+                            <input class="input-field" type="text" name="rua">
+                            <label for="rua">Rua</label>
                         </div>
                         <div class="input-field col s12">
-
+                            <input class="input-field" type="text" name="numero">
+                            <label for="numero">Número da casa</label>
+                        </div>
+                        <div class="input-field col s12">
+                            <input class="input-field" type="text" name="cpf">
+                            <label for="cpf">CPF</label>
+                        </div>
+                        <div class="input-field col s12">
+                            <input class="input-field" type="text" name="rg">
+                            <label for="rg">RG</label>
+                        </div>
+                        <div class="input-field col s12">
+                            <input class="input-field" type="text" name="telefone">
+                            <label for="telefone">Telefone</label>
                         </div>
                         <div class="input-field col s12">
 
@@ -54,19 +69,6 @@ session_start();
 
                         <!--
                         
-                        
-                        <input class="input-field" type="text" name="bairro">
-                        <label for="bairro">Bairro</label>
-                        <input class="input-field" type="text" name="rua">
-                        <label for="rua">Rua</label>
-                        <input class="input-field" type="text" name="numero">
-                        <label for="numero">Número</label>
-                        <input class="input-field" type="text" name="cpf">
-                        <label for="cpf">CPF</label>
-                        <input class="input-field" type="text" name="rg">
-                        <label for="rg">RG</label>
-                        <input class="input-field" type="text" name="telefone">
-                        <label for="telefone">Telefone</label>
                         <input class="input-field" type="text" name="email">
                         <label for="email">E-mail</label>
                         -->
