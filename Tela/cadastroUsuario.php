a7b729918a1e2d7c948c14e768e4ceab81ddb676
Lucas Lima <lucaslimasilva1998@gmail.com>
2019-03-21 21:16:22 -0300

    1.0.2

--- Tela/cadastroUsuario.php
+++ Tela/cadastroUsuario.php
@@ -28,69 +28,61 @@ session_start();
                             <input class="input-field" type="text" name="nome">
                             <label for="nome">Nome completo</label>
                         </div>
-                        <div class="input-field col s12">
-                            <input class="input-field" type="text" name="login">
-                            <label for="login">Login</label>
-                        </div>
+                       
                         <div class="input-field col s12">
                             <input class="input-field" type="text" name="cidade">
                             <label for="cidade">Cidade</label>
                         </div>
-                        <div class="input-field col s12">
-                            <input class="input-field" type="text" name="bairro">
-                            <label for="bairro">Bairro</label>
-                        </div>
+                        
                         <div class="input-field col s12">
                             <input class="input-field" type="text" name="rua">
                             <label for="rua">Rua</label>
                         </div>
-                        <div class="input-field col s12">
-                            <input class="input-field" type="text" name="numero">
-                            <label for="numero">Número da casa</label>
-                        </div>
+                        
                         <div class="input-field col s12">
                             <input class="input-field" type="text" name="cpf">
                             <label for="cpf">CPF</label>
                         </div>
-                        <div class="input-field col s12">
-                            <input class="input-field" type="text" name="rg">
-                            <label for="rg">RG</label>
-                        </div>
+                        
                         <div class="input-field col s12">
                             <input class="input-field" type="text" name="telefone">
                             <label for="telefone">Telefone</label>
                         </div>
+                       
                         <div class="input-field col s12">
 
                         </div>
-                        <div class="input-field col s12">
-
-                        </div>
-
-                        <!--
-                        
-                        <input class="input-field" type="text" name="email">
-                        <label for="email">E-mail</label>
-                        -->
-
                     </div>
-                    <!--
                     <div class="input-field col s6">
-                        <input class="input-field" type="text" name="nome">
-                        <label for="nome">Lucas</label>
-                        <input class="input-field" type="text" name="nome">
-                        <label for="nome">Lucas</label>
-                        <input class="input-field" type="text" name="nome">
-                        <label for="nome">Lucas</label>
-                        <input class="input-field" type="text" name="nome">
-                        <label for="nome">Lucas</label>
-                        <input class="input-field" type="text" name="nome">
-                        <label for="nome">Lucas</label>
-                        <input class="input-field" type="text" name="nome">
-                        <label for="nome">Lucas</label>
-                    </div>  
-                    -->
+                        
+                        <div class="input-field col s12">
+                            <input class="input-field" type="text" name="login">
+                            <label for="login">Login</label>
+                        </div>
+                        
+                        <div class="input-field col s12">
+                            <input class="input-field" type="text" name="bairro">
+                            <label for="bairro">Bairro</label>
+                        </div>
+                        
+                        <div class="input-field col s12">
+                            <input class="input-field" type="text" name="numero">
+                            <label for="numero">Número da casa</label>
+                        </div>
+                        
+                        <div class="input-field col s12">
+                            <input class="input-field" type="text" name="rg">
+                            <label for="rg">RG</label>
+                        </div>
+                        
+                        <div class="input-field col s12">
+                            <input class="input-field" type="text" name="email">
+                            <label for="email">E-mail</label>
+                        </div>
+                        <div class="input-field col s12">
 
+                        </div>
+                    </div>  
                 </form>
             </div>
             <div class="col s3"></div>
