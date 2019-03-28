c474f9bb72c16a6de95fb635d9462adffc586822
Eu sou o Matrix <Eu sou o Matrix@200.132.17.125>
2019-03-27 21:14:47 -0300

    V 1.0.11

--- associacao-1-0-2.sql
+++ associacao-1-0-2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 26-Mar-2019 às 14:40
--- Versão do servidor: 10.1.36-MariaDB
--- versão do PHP: 7.2.10
+-- Generation Time: 28-Mar-2019 às 01:14
+-- Versão do servidor: 10.1.38-MariaDB
+-- versão do PHP: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -31,18 +31,11 @@ SET time_zone = "+00:00";
 CREATE TABLE `aluno` (
   `id_usuario` int(11) NOT NULL,
   `id_responsavel` int(11) DEFAULT NULL,
-  `data_nasc` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `curso` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `data_nasc` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `curso` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `saldo` decimal(10,2) DEFAULT NULL,
-  `previsao_coclusao` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Extraindo dados da tabela `aluno`
---
-
-INSERT INTO `aluno` (`id_usuario`, `id_responsavel`, `data_nasc`, `curso`, `saldo`, `previsao_coclusao`) VALUES
-(1, 2, '00/00/0000', 'ADS', '15000.00', '00/00/0000');
+  `previsao_conclusao` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -52,15 +45,8 @@ INSERT INTO `aluno` (`id_usuario`, `id_responsavel`, `data_nasc`, `curso`, `sald
 
 CREATE TABLE `diretoria` (
   `id_usuario` int(11) NOT NULL,
-  `cargo` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Extraindo dados da tabela `diretoria`
---
-
-INSERT INTO `diretoria` (`id_usuario`, `cargo`) VALUES
-(1, 'Presidente');
+  `cargo` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -70,21 +56,21 @@ INSERT INTO `diretoria` (`id_usuario`, `cargo`) VALUES
 
 CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
-  `nome` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `usuario` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `senha` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
-  `cidade` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `bairro` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `rua` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `numero` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `cep` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  `cpf` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `rg` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `telefone` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `email` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `pode_logar` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `administrador` varchar(50) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `nome` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `usuario` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `senha` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cidade` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `bairro` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `rua` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `numero` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `cep` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cpf` varchar(11) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `rg` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `telefone` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `pode_logar` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `administrador` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Extraindo dados da tabela `usuario`
@@ -92,7 +78,8 @@ CREATE TABLE `usuario` (
 
 INSERT INTO `usuario` (`id`, `nome`, `usuario`, `senha`, `cidade`, `bairro`, `rua`, `numero`, `cep`, `cpf`, `rg`, `telefone`, `email`, `pode_logar`, `administrador`) VALUES
 (1, 'Teste', 'teste', '202cb962ac59075b964b07152d234b70', 'Teste', 'Teste', 'Teste', 'Teste', 'asd', 'asd', 'asd', 'asd', 'asd', 'true', 'true'),
-(2, 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a');
+(2, 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'),
+(3, 'Daniel Zanini de Castro', 'dcastro', '3c2031ac53dea3dacb733041d55e322d', 'Jaguari', 'Centro', 'Av. dr. severiano de almeida n 280', '5', '97760-000', '039.855.650', '56489894', '5599598414', 'zanini.castro@hotmail.com', 'true', 'false');
 
 --
 -- Indexes for dumped tables
@@ -125,7 +112,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
