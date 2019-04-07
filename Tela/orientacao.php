1ed4b31f7b17face2c47e652eaee998253034607
Lucas Lima <lucaslimasilva1998@gmail.com>
2019-04-07 10:28:48 -0300

    1.0.8.10 - Modificações gerais

--- Tela/orientacao.php
+++ Tela/orientacao.php
@@ -1,7 +1,6 @@
 <?php
-
 if (!isset($_SESSION)) {
-     session_start();
+    session_start();
 }
 if (!isset($_SESSION['id'])) {
     header("Location: ./login.php");
@@ -14,22 +13,35 @@ if (!isset($_SESSION['id'])) {
     </head> 
     <body class="homeimg">
         <?php
+        if ($_SESSION['adminstrador']=='true') {
+            include_once '../Base/navAdministrativa.php';
+        }else{
+            if ($_SESSION['adminstrador']=='false') {
+                include_once '../Base/navPadrao.php';
+            }
+        }
         if (isset($_GET['msg'])) {
-            if ($_GET['msg']=='sucessoAluno') {
+            if ($_GET['msg'] == 'sucessoAluno') {
                 ?>
                 aluno cadastrado com sucesso - Maior de idade
                 <?php
-            }else{
-                if ($_GET['msg']=='sucessoMenorDeIdade') {
+            } else {
+                if ($_GET['msg'] == 'sucessoAlunoRequerimento') {
                     ?>
-                aluno menor de idade cadastrado com sucesso - Deseja cadastrar um responsável?
-                <?php
+                    aluno menor de idade cadastrado com sucesso - Deseja cadastrar um responsável?
+                    <?php
+                } else {
+                    if ($_GET['msg'] == 'cadastrarResponsavel') {
+                        ?>
+                        Form cadastro de responsáveel
+                        <?php
+                    }
                 }
             }
-        }else{
+        } else {
             header("Location: ../index.php");
         }
         ?>
-        
+
     </body>
 </html>
