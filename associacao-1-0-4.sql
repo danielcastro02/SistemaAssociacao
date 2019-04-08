750a185baf016e9a911ab7f1531dfb11ca89e305
2017007243 <2017007243@lab5maq06.svs.iffarroupilha.local>
2019-04-08 16:41:34 -0300

    Agora Funciona

---
+++ associacao-1-0-4.sql
@@ -0,0 +1,148 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 08-Abr-2019 às 21:41
+-- Versão do servidor: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `associacao`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `aluno`
+--
+
+CREATE TABLE IF NOT EXISTS `aluno` (
+  `id_usuario` int(11) NOT NULL,
+  `id_responsavel` int(11) DEFAULT NULL,
+  `curso` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `saldo` decimal(10,2) DEFAULT NULL,
+  `previsao_coclusao` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `aluno`
+--
+
+INSERT INTO `aluno` (`id_usuario`, `id_responsavel`, `curso`, `saldo`, `previsao_coclusao`) VALUES
+(1, 2, 'ADS', '15000.00', '00/00/0000'),
+(4, 7, 'ADS', '0.00', '2022-01-01');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `diretoria`
+--
+
+CREATE TABLE IF NOT EXISTS `diretoria` (
+  `id_usuario` int(11) NOT NULL,
+  `cargo` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `diretoria`
+--
+
+INSERT INTO `diretoria` (`id_usuario`, `cargo`) VALUES
+(1, 'Presidente');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE IF NOT EXISTS `usuario` (
+`id` int(11) NOT NULL,
+  `nome` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `usuario` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `senha` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `cidade` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `bairro` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `rua` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `numero` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `cep` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `cpf` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `rg` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `data_nasc` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `telefone` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `pode_logar` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `administrador` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `nome`, `usuario`, `senha`, `cidade`, `bairro`, `rua`, `numero`, `cep`, `cpf`, `rg`, `data_nasc`, `telefone`, `email`, `pode_logar`, `administrador`) VALUES
+(1, 'Teste', 'teste', '202cb962ac59075b964b07152d234b70', 'Teste', 'Teste', 'Teste', 'Teste', 'asd', 'asd', 'asd', '', 'asd', 'asd', 'true', 'true'),
+(2, 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', '', 'a', 'a', 'false', 'a'),
+(4, 'Daniel Zanini de Castro', 'dcastro', '3c2031ac53dea3dacb733041d55e322d', 'Jaguari', 'Centro', 'Av. dr. severiano de almeida n 280', '280', '97760-000', '039.855.650', '56489894', '2008-01-01', '5599598414', 'zanini.castro@hotmail.com', 'false', 'false'),
+(7, 'Jose daltro', 'jda', '698dc19d489c4e4db73e28a713eab07b', 'Jaguari', 'Centro', 'Av. dr. severiano de almeida n 280', '280', '97760-000', '59876303', '0946787', '1985-01-31', '5599598414', 'teste@gmail.com', 'false', 'false');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `aluno`
+--
+ALTER TABLE `aluno`
+ ADD PRIMARY KEY (`id_usuario`), ADD KEY `id_responsavel` (`id_responsavel`);
+
+--
+-- Indexes for table `diretoria`
+--
+ALTER TABLE `diretoria`
+ ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `aluno`
+--
+ALTER TABLE `aluno`
+ADD CONSTRAINT `aluno_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`),
+ADD CONSTRAINT `aluno_ibfk_2` FOREIGN KEY (`id_responsavel`) REFERENCES `usuario` (`id`);
+
+--
+-- Limitadores para a tabela `diretoria`
+--
+ALTER TABLE `diretoria`
+ADD CONSTRAINT `diretoria_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
