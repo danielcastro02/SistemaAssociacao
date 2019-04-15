228f7fba82629224ad73497dfafeb34a15a3ca1a
Konrado <Konrado@DESKTOP-VHO1N4T.lan>
2019-04-15 11:18:19 -0300

    1.0.8

---
+++ Controle/proc_cad_img.php
@@ -0,0 +1,53 @@
+<?php
+
+if (!isset($_SESSION)) {
+    session_start();
+}
+if (!isset($_SESSION['usuario'])) {
+    header('Location: ../Tela/login.php');
+}
+include_once '../Modelo/usuario.php';
+$user = new usuario();
+include_once './conexao.php';
+
+$SendCadImg = filter_input(INPUT_POST, 'SendCadImg', FILTER_SANITIZE_STRING);
+if ($SendCadImg) {
+    //Receber os dados do formulário
+    $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
+    $nome_imagem = $_FILES['imagem']['name'];
+    //var_dump($_FILES['imagem']);
+    //Inserir no BD
+    $conexao = new conexao();
+    $pdo = $conexao->getConexao();
+    $sql = $pdo->prepare("INSERT INTO imagens (default, id_usuario, nome, imagem) VALUES(default, :id_usuario, :nome, :imagem)");
+    $sql->bindParam(':id_usuario', $user->getId());
+    $sql->bindParam(':nome', $nome);
+    $sql->bindParam(':imagem', $nome_imagem);
+
+    //Verificar se os dados foram inseridos com sucesso
+    if ($sql->execute()) {
+        //Recuperar último ID inserido no banco de dados
+        //$ultimo_id = $pdo->lastInsertId();
+        $ultimo_id = 1;
+
+        //Diretório onde o arquivo vai ser salvo
+        $diretorio = 'Img/' . $ultimo_id . '/';
+
+        //Criar a pasta de foto
+        mkdir($diretorio, 0755);
+
+        if (move_uploaded_file($_FILES['imagem']['tmp_name'], $diretorio . $nome_imagem)) {
+            $_SESSION['msg'] = "<p style='color:green'>Dados salvos com sucesso e upload da imagem realizado com sucesso.</p>";
+            header('Location: ../Tela/alterarDadosUsuario.php');
+        } else {
+            $_SESSION['msg'] = "<p><span style='color:green'>Dados salvos com sucesso. </span><span style='color:red;'>Erro ao realizar o upload da imagem.</span></p>";
+            header('Location: ../Tela/loginRecusado.php');
+        }
+    } else {
+        $_SESSION['msg'] = "<p style='color:red'>Erro ao salvar os dados</p>";
+        header('Location: ../Tela/alterarCurso.php');
+    }
+} else {
+    $_SESSION['msg'] = "<p style='color:red'>Erro ao salvar os dados</p>";
+    header('Location: ../Tela/alterarEnderecoUsuario.php');
+}
